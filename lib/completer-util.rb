e13e32fd91dbb92118d1e19e2a297c907add720b
Olivier PEYRUSSE <olivier.peyrusse@mobile-devices.fr>
2014-01-27 12:58:44 +0100

    Remove trailing whitespaces.

--- lib/completer-util.rb
+++ lib/completer-util.rb
@@ -3,16 +3,16 @@
 module Completer
 
   class Completer
-  
+
     def initialize &definitions
       return unless completion_phase?
 
       @completions = {}
       @aliases = {}
-  
+
       instance_eval(&definitions)
       collect_options
-  
+
       exit do_completion
     end
 
@@ -25,10 +25,10 @@ module Completer
       @completions[ACTIONS_KEY] = action
       @aliases[ACTIONS_KEY] = ACTIONS_KEY
     end
-  
+
     def complete_options *values, &action
       raise ArgumentError.new "Invalid empty array of options" unless values && values.size > 0
-  
+
       @completions[values.first] = action
       values.each { |option| @aliases[option] = values.first }
     end
@@ -40,21 +40,21 @@ module Completer
         return 1, *(@aliases.keys)
       }
     end
-  
+
     def do_completion
       parse_arguments
-  
+
       # Get the appropriate action
       return 3 unless @completion_key
       completion_action = @completions[@completion_key]
-       
+
       exit_code, *values = completion_action.call @stream
       puts values.join " "
-  
+
       return exit_code
     end
-  
-    def parse_arguments 
+
+    def parse_arguments
       i = 0
       while i < ARGV.size
         arg = ARGV[i]
@@ -76,7 +76,7 @@ module Completer
     def uncased_context value
       value.dup.gsub /@/, '-'
     end
-  
+
   end
 
 end
